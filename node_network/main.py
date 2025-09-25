17151463ad9fc531c03d0a6913367801a604e918
PW-Hansen <Peterweishansen@Hotmail.com>
2025-09-25 18:33:51 +0200

    Update main.py
    Oops, this was meant to be the last push.

--- node_network/main.py
+++ node_network/main.py
@@ -1,50 +1,28 @@
 from classes import Vertex, Edge
-from functions_building import load_osm_json, build_graph
 from functions_building import build_graph
 from functions_misc import output_network_distances
 from functions_analysis import find_networks, filter_non_largest_network, bin_edges_by_lat_lon, bin_vertices_by_lat_lon
 from functions_mapping import project_trip_coordinates_onto_edges, apply_projection_and_splitting, all_pairs_network_distances_between_layers
-from functions_plotting import plot_networks
 from functions_plotting import plot_networks, plot_directed_graph
 import matplotlib.pyplot as plt
 import json
 import os
 
 if __name__ == "__main__":
-
-    networks = find_networks(list(Vertex.vertex_dict.values()))
-    filter_non_largest_network(networks)
-    edges_binned_matrix = bin_edges_by_lat_lon()  # Build edges_binned_matrix after filtering
-    vertices_binned_matrix = bin_vertices_by_lat_lon()
-    results = project_trip_coordinates_onto_edges(edges_binned_matrix, trip_id=6, trip_file='trips_150103.csv',
-                                                  cell_range=1, max_dist=50)
-
-    print(f"Number of vertices before splitting: {len(Vertex.vertex_dict)}")
-    changes, vertex_layers = apply_projection_and_splitting(vertices_binned_matrix, results)
-    print(f"Number of vertices after splitting: {len(Vertex.vertex_dict)}")
-
-    trip = 'trips_150103_6'
-    data_output_dict = {
-        'transitions':  {
-            trip: {}
-        }
-    }
-
-
-    for i in range(len(vertex_layers)-1):
-        result = all_pairs_network_distances_between_layers(vertex_layers[i], vertex_layers[i+1])        
-        transition = {}
-        for k, v in result.items():
-            transition[k.id] = {vv.id: dist for vv, dist in v.items()}
-        data_output_dict['transitions'][trip][i] = transition
-
-    output_path = 'peter_fucking_around/output_data/network_distances.json'
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    with open(output_path, 'w') as f:
-        json.dump(data_output_dict, f, indent=4)
     build_graph()
+    
+    #networks = find_networks(list(Vertex.vertex_dict.values()))
+    #filter_non_largest_network(networks)
+    # edges_binned_matrix = bin_edges_by_lat_lon()  # Build edges_binned_matrix after filtering
+    #vertices_binned_matrix = bin_vertices_by_lat_lon()
+    #results = project_trip_coordinates_onto_edges(edges_binned_matrix, trip_id=6, trip_file='trips_150103.csv',
+    #                                              cell_range=1, max_dist=50)
+
+    #print(f"Number of vertices before splitting: {len(Vertex.vertex_dict)}")
+    #changes, vertex_layers = apply_projection_and_splitting(vertices_binned_matrix, results)
+    #print(f"Number of vertices after splitting: {len(Vertex.vertex_dict)}")
 
     # Plot as usual
     # plot_networks(networks, edges_binned_matrix, results, highlight_lat=45.7485, highlight_lon=126.6905, show_densest_bin=True)
-    # plot_networks(edges_binned_matrix, results, vertex_layers)
+    # plot_networks()
+    plot_directed_graph(45.72, 45.77, 126.72, 126.75)
