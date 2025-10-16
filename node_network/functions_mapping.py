26c2d41e5401e8db87aa6fc7e79be545d1a15ad9
Peter <Peterweishansen@Hotmail.com>
2025-10-16 11:31:00 +0200

    no unnecessary output writing, dummy

--- node_network/functions_mapping.py
+++ node_network/functions_mapping.py
@@ -360,9 +360,4 @@ def process_trip(lats, lons, cell_range=0, max_dist=50):
         return None
     data_output_dict = generate_network_distances_dict(vertex_layers)
 
-    output_path = f'peter_fucking_around/output_data/network_distances.json'
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    with open(output_path, 'w') as f:
-        json.dump(data_output_dict, f, indent=4)
-
     return data_output_dict
