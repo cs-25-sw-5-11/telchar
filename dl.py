5b2a4761ca6ea249557841469a6af27387282122
Mads Fagerlund <mads@fagerlund.io>
2025-12-09 12:13:40 +0100

    update

--- dl.py
+++ dl.py
@@ -53,7 +53,7 @@ def save_graph_shapefile_directional(
 if __name__ == "__main__":
     print("osmnx version", ox.__version__)
 
-    df: pd.DataFrame = pd.read_csv("data/cleaned_data/trips_150103.csv", sep=";")
+    df: pd.DataFrame = pd.read_csv("data/cleaned_data/trips_150103.csv", sep=",")
 
     y1: float = df["latitude"].max() + 0.01
     y2: float = df["latitude"].min() - 0.01
